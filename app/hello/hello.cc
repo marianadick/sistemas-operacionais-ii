483d04bc568cdbc350d0ffc1f7e53abdcf6efe5c
Larissa Gremelmaier Rosa <larissagr101@gmail.com>
2023-04-09 14:09:09 -0300

    add new test to 6e

--- app/hello/hello.cc
+++ app/hello/hello.cc
@@ -5,43 +5,40 @@ using namespace EPOS;
 
 OStream cout;
 
+
+
 int main()
 {
-    cout << "Starting E6b worst fit alocation test" << endl;
+    cout << "Starting E6b worst fit allocation test" << endl;
 
-    // Especifica tamanho da heap manualmente
-    const unsigned int HEAP_SIZE = 4096;
-
-    // Aloca espaço para a heap na memória
+    const unsigned int HEAP_SIZE = 4096*2;
     char heap_buffer[HEAP_SIZE];
 
-    // Inicializa a heap
-    Heap heap(reinterpret_cast<void*>(heap_buffer), HEAP_SIZE);
+   
+    Heap heap(heap_buffer, HEAP_SIZE);
 
-    char **array = new char*[4];
-    cout << "Endereço inicial do array: " << array << endl;
+    char **array = new char*[3];
+    cout << "Initial address of the array: " << array << endl;
 
-    array[0] = reinterpret_cast<char *>(heap.alloc(512));
-    cout << "Alocando 512 bytes - array[0] = " << static_cast<void*>(array[0]) << endl; // imprime o endereço de memória
+    array[0] = reinterpret_cast<char *>(heap.alloc(1024));
+    cout << "Allocating 1024 bytes - array[0] = " << static_cast<void*>(array[0]) << endl;
+    array[1] = reinterpret_cast<char *>(heap.alloc(2048));
+    cout << "Allocating 2048 bytes - array[1] = " << static_cast<void*>(array[1]) << endl;
+    array[2] = reinterpret_cast<char *>(heap.alloc(3072));
+    cout << "Allocating 3072 bytes - array[2] = " << static_cast<void*>(array[2]) << endl;
+    
+    heap.free(reinterpret_cast<void *>(array[1]), 2048);
+    cout << "After freeing array[1], heap size = " << heap.grouped_size() << " bytes" << endl;
+
     array[1] = reinterpret_cast<char *>(heap.alloc(512));
-    cout << "Alocando 512 bytes - array[1] = " << static_cast<void*>(array[1]) << endl; // imprime o endereço de memória
-    array[2] = reinterpret_cast<char *>(heap.alloc(2048));
-    cout << "Alocando 2048 bytes - array[2] = " << static_cast<void*>(array[2]) << endl; // imprime o endereço de memória
-    array[3] = reinterpret_cast<char *>(heap.alloc(512));
-    cout << "Alocando 512 bytes - array[3] = " << static_cast<void*>(array[3]) << endl; // imprime o endereço de memória
-
-    heap.free(reinterpret_cast<void *>(array[2]), 2048);
-    cout << "After freeing array[2], heap size = " << heap.grouped_size() << " bytes" << endl;
-
-    array[2] = reinterpret_cast<char *>(heap.alloc(512));
-    cout << "Alocando 512 bytes - array[2] = " << static_cast<void*>(array[2]) << endl; // imprime o endereço de memória
+    cout << "Allocating 512 bytes - array[1] = " << static_cast<void*>(array[1]) << endl;
     cout << "Heap size: " << heap.grouped_size() << " bytes" << endl;
 
-    heap.free(reinterpret_cast<void *>(array[0]), 512);
+    heap.free(reinterpret_cast<void *>(array[0]), 1024);
     heap.free(reinterpret_cast<void *>(array[1]), 512);
-    heap.free(reinterpret_cast<void *>(array[2]), 512);
-    heap.free(reinterpret_cast<void *>(array[3]), 512);
-
-    cout << "After freeing array[0] to and array[3], heap size = " << heap.grouped_size() << " bytes" << endl;
+    heap.free(reinterpret_cast<void *>(array[2]), 3072);
+    
+    cout << "After freeing array[0] to array[2], heap size = " << heap.grouped_size() << " bytes" << endl;
     return 0;
 }
+
